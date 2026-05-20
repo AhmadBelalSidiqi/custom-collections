a79108f3eb91b627bc3badb5f2fbfa1443bf24a3
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-20 14:43:28 -0400

    FixedList created. exercise complete.

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,22 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private final List<T> items;
+    private final int maxSize;
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+    public void add(T item){
+        if (items.size()<maxSize)
+            items.add(item);
+    }
+
+    public List<T> getItems(){
+        return this.items;
+    }
 }
